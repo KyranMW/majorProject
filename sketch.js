6027d9af7ed5e924418c4653ffb485be913ecf5f
KyranMW <KyranMW@users.noreply.github.com>
2021-02-10 12:39:19 -0600

    added graphics for grid and arrow buttons

--- sketch.js
+++ sketch.js
@@ -8,6 +8,15 @@ let rows;
 let cols;
 let cellWidth;
 let cellHeight;
+let gridPattern, rightArrow, downArrow, leftArrow, upArrow;
+
+function preload(){
+  gridPattern = loadImage("assets/grid pattern.png");
+  rightArrow = loadImage("assets/right arrow.png");
+  leftArrow = loadImage("assets/left arrow.png");
+  upArrow = loadImage("assets/up arrow.png");
+  downArrow = loadImage("assets/down arrow.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -24,17 +33,25 @@ function setup() {
 function draw() {
   background(220);
   translate(width/2 - 100, height/2 - 100);
+  rect(-1,0,201,201);
   drawGrid(cols, rows);
-  
+  drawArrows();
 }
 
 function drawGrid(cols, rows){
   for (let y=0; y<rows; y++) {
     for (let x=0; x<cols; x++) {
       fill("white");
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      image(gridPattern, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
+}
+
+function drawArrows(){
+  image(upArrow, 75, 210, cellWidth, cellHeight);
+  image(downArrow, 75, 260, cellWidth, cellHeight);
+  image(rightArrow, 125, 260, cellWidth, cellHeight);
+  image(leftArrow, 25, 260, cellWidth, cellHeight);
 }
 
 function mousePressed() {
