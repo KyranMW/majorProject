3cfa5727bb0597812f67e89988d8e308d3b26b72
KyranMW <KyranMW@users.noreply.github.com>
2021-02-11 14:05:38 -0600

    started creating a class for numbers in 2048

--- sketch.js
+++ sketch.js
@@ -2,8 +2,6 @@
 // Kyran Mcknight-Woods
 // February 2nd, 2021
 
-let grid;
-
 let rows;
 let cols;
 let cellWidth;
@@ -21,9 +19,6 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-
-  grid = createEmptyGrid(30, 20);
-
   rows = 4;
   cols = 4;
   cellWidth = 50;
@@ -38,10 +33,20 @@ function draw() {
   drawArrows();
 }
 
+class num {
+  constructor(){
+    this.x = random(0,3) * cellWidth;
+    this.y = random(0,3) * cellHeight;
+    this.dx = 0.5;
+    this.dy = 0.5;
+    this.width = cellWidth;
+    this.height = cellHeight;
+  }
+}
+
 function drawGrid(cols, rows){
   for (let y=0; y<rows; y++) {
     for (let x=0; x<cols; x++) {
-      fill("white");
       image(gridPattern, x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
@@ -52,27 +57,4 @@ function drawArrows(){
   image(downArrow, 75, 260, cellWidth, cellHeight);
   image(rightArrow, 125, 260, cellWidth, cellHeight);
   image(leftArrow, 25, 260, cellWidth, cellHeight);
-}
-
-function mousePressed() {
-  let x = Math.floor(mouseX / cellWidth);
-  let y = Math.floor(mouseY / cellHeight);
-
-  if (grid[y][x] === 1) {
-    grid[y][x] = 0;
-  }
-  else if (grid[y][x] === 0) {
-    grid[y][x] = 1;
-  }
-}
-
-function createEmptyGrid(cols, rows) {
-  let emptyGrid = [];
-  for (let y=0; y<rows; y++) {
-    emptyGrid.push([]);
-    for (let x=0; x<cols; x++) {
-      emptyGrid[y].push(0);
-    }
-  }
-  return emptyGrid;
 }
